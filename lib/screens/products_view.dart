f807dfaa7490a8b9cf4280784b7648adbfdca1b6
melosaa <melosakaya@outlook.com>
2024-07-22 22:37:25 +0300

    profile detail design

--- lib/screens/products_view.dart
+++ lib/screens/products_view.dart
@@ -1,15 +1,19 @@
 import 'package:auto_route/annotations.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:melisa_store/blocs/product_detail/bloc/product_detail_bloc.dart';
 
 @RoutePage()
 class ProductDetailView extends StatelessWidget {
   const ProductDetailView({super.key, required this.id});
   final int id;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text("Products"),
       ),
@@ -39,6 +43,108 @@ class _renderProductDetail extends StatelessWidget {
   ProductDetailSucces state;
   @override
   Widget build(BuildContext context) {
-    return Text(state.product.title);
+    return Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Text(
+          //   state.product.title,
+          //   style: Theme.of(context).textTheme.headlineMedium,
+          // ),
+          Center(
+            child: Container(
+              color: const Color.fromARGB(255, 246, 246, 246),
+              child: Image.network(state.product.image),
+              height: 375,
+              width: 238,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              Text(
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                state.product.title,
+                style: const TextStyle(
+                    color: Color.fromARGB(255, 34, 50, 99),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(width: 160),
+              Icon(CupertinoIcons.heart),
+            ],
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          RatingBar.builder(
+              initialRating: 4,
+              minRating: 1,
+              direction: Axis.horizontal,
+              allowHalfRating: true,
+              itemCount: 5,
+              itemBuilder: (context, _) => const Icon(
+                    Icons.star,
+                    color: Color.fromARGB(255, 255, 200, 52),
+                  ),
+              onRatingUpdate: (rating) {}),
+          const SizedBox(
+            height: 8,
+          ),
+          Text(
+            " \$${state.product.price.toString()}",
+            style: const TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 88, 199, 255)),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                width: 60,
+                height: 80,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.grey.shade200)),
+                child: const CircleAvatar(
+                  radius: 68,
+                  backgroundColor: Colors.white,
+                  child: Text("S"),
+                ),
+              ),
+              Container(
+                width: 60,
+                height: 80,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.grey.shade200)),
+                child: const CircleAvatar(
+                  radius: 68,
+                  backgroundColor: Colors.white,
+                  child: Text("M"),
+                ),
+              ),
+              Container(
+                width: 60,
+                height: 80,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.grey.shade200)),
+                child: const CircleAvatar(
+                  radius: 68,
+                  backgroundColor: Colors.white,
+                  child: Text("L"),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
